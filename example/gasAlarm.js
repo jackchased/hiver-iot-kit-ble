0762027f7dd0d5dec066a785f5927bf58043c633
Eason <onepiece5478@gmail.com>
2017-01-10 11:06:12 +0800

    Update gasAlarm.js

--- example/gasAlarm.js
+++ example/gasAlarm.js
@@ -2,7 +2,7 @@
 var _ = require('busyman'),
     chalk = require('chalk');
 
-var gasSensorPlugin = require('bshep-plugin-sivann-gassensor'); 
+var gasSensorPlugin = require('bshep-plugin-sivann-gassensor');
 
 function app (central) {
     central.support('gasSensor', gasSensorPlugin); // give a device name to the module you are going to use. This name will be used in further applications.
@@ -30,74 +30,80 @@ function app (central) {
 
 /**********************************/
 /* BLE Application                */
-/**********************************/   
+/**********************************/
 var gasSensor, gasSensor1, gasSensor2;
 function bleApp (central) {
 	var blocker = central.blocker;
-    
+
 	/*** add your devices to blacklist ***/
 	//blocker.enable('black');			// enable blacklist service. Use blacklist to ban a known devices.
     //blocker.block('0x5c313e2bfb34');	// ban a specified device by its MAC address
- 
-	/*** add your devices to whitelist ***/	
+
+	/*** add your devices to whitelist ***/
     //blocker.enable('white');         		// enable whitelist service. Use whitelist to block other unknown/unwanted BLE devices, and only specified devices can join your network.
 	//blocker.unblock('0x689e192a8c5e');	// specify a device to join the network by using its MAC address
 	//blocker.unblock('0x20914838225b');
 
-    central.permitJoin(60);             // 60s the default value to allow devices joining the network. 
+    central.permitJoin(60);             // 60s the default value to allow devices joining the network.
     central.on('ind', function(msg) {
 		var dev = msg.periph;
 		
 		switch (msg.type) {
-            /*** devIncoming      ***/        
+            /*** devIncoming      ***/
 			case 'devIncoming':
-                if (dev.name)  
-                    console.log(chalk.yellow('[   devIncoming ] ') + '@' + dev.addr + ', ' + dev.name + ', firmware ' + dev.findChar('0x180a', '0x2a26').value.firmwareRev); // display the device MAC and name. Use this MAC address for blacklist or whitelist. 
+                if (dev.name)
+                    console.log(chalk.yellow('[   devIncoming ] ') + '@' + dev.addr + ', ' + dev.name + ', firmware ' + dev.findChar('0x180a', '0x2a26').value.firmwareRev); // display the device MAC and name. Use this MAC address for blacklist or whitelist.
                 else
                     console.log(chalk.yellow('[   devIncoming ] ') + '@' + dev.addr + ', failed to recognize this incoming device.');
-                                  
+
 				if(dev.name === 'gasSensor') {
-					gasSensor = dev;					                  
-                    /***  write your application here   ***/          
-                    
+					gasSensor = dev;
+                    /***  write your application here   ***/
+
                     // you can call the private function to enable all the indication/notification of each Characteristic automatically.                    
                     configNotifyAll(gasSensor); 
                     // you can also manually enable or disable the indication/notification of each Characteristic.
 					// gasSensor.configNotify('0xbb60', '0xcc28', true);	// buzzer
 					// gasSensor.configNotify('0xbb50', '0xcc04', true);	// gas
-                    gasSensor.configNotify('0xbb00', '0xcc00', false);	// DIn  Set to false to disable the notification 
-                    gasSensor.configNotify('0xbb10', '0xcc02', false);	// AIn  Set to false to disable the notification
-                    
+                    gasSensor.configNotify('0xbb00', '0xcc00', false);	// DIn
+                    gasSensor.configNotify('0xbb10', '0xcc02', false);	// AIn
+
                     // Register your handler to handle notification or indication of each Characteristic.
 					gasSensor.onNotified('0xbb60', '0xcc28', buzzerHdlr);	// buzzer
 					gasSensor.onNotified('0xbb50', '0xcc04', gasHdlr);		// gas
                     gasSensor.onNotified('0xbb00', '0xcc00', callbackDIn);	// DIn
-                    gasSensor.onNotified('0xbb10', '0xcc02', callbackAIn);	// AIn                    
+                    gasSensor.onNotified('0xbb10', '0xcc02', callbackAIn);	// AIn
                     gasSensor.write('0xbb50', '0xbb52', {period: 250}, function (err) {
-                        if (err) 
+                        if (err)
                             console.log(chalk.red('[         error ]') + ' failed to change the period. ' + err);
-                        else 
-                            console.log('[ debug message ] changed the reporting period to 2.5s.'); // (recommend range: 100-255)
+                        else {
+                            gasSensor.read('0xbb50', '0xbb52', function (err, value) {
+                                if (err)
+                                  console.log(chalk.red('[         error ]') + ' failed to read period. ' + err);
+                                else
+                                  console.log('[ debug message ] changed the reporting period to ' + value.period / 100 + 's.'); // (recommend range: 100-255)
+                            });
+                        }
                     });
 					gasSensor.write('0xbb50', '0xbb53', {option: 1}, function (err) { // option 0:Propane(default), 1:Smoke, 2:Methane, 3:Ethanol
-                        if (err) 
+                        if (err)
                             console.log(chalk.red('[         error ]') + ' failed to change the option. ' + err);
                         else 
-                            console.log('[ debug message ] changed the measuring gas option to Smoke.'); 
+                            console.log('[ debug message ] changed the measuring gas option to Smoke.');
                     });
-					gasSensor.write('0xbb50', '0xbb54', {threshold: 500}, function (err) { // threshold range: 10-10000
+					gasSensor.write('0xbb50', '0xbb54', {threshold: 500}, function (err) { // threshold range: 100-10000
                         if (err)
                             console.log(chalk.red('[         error ]') + ' failed to change the threshold. ' + err);
                         else 
-                            console.log('[ debug message ] changed the gas threshold value to 500 ppm.');  
+                            console.log('[ debug message ] changed the gas threshold value to 500 ppm.');
                     });
-					
+
                     //gasSensor.write('0xbb50', '0xbb51', {config : false});    // uncomment to turn off gas measurements.
-                    
-                    /*** you will have to switch case between device addresses only if you have multiple gas sensor modules. ***/       
+
+                    /*** you will have to switch case between device addresses only if you have multiple gas sensor modules. ***/
 /*                    switch (dev.addr) {
-                        case '0x689e192a8c5e':                          
-                            //  write your application for the 1st gas sensor  //                        
+                        case '0x689e192a8c5e':
+                            //  write your application for the 1st gas sensor  //
                             gasSensor1 = dev;
                             configNotifyAll(gasSensor1);
                             gasSensor1.onNotified('0xbb60', '0xcc28', buzzerHdlr); // buzzer
@@ -105,29 +111,41 @@ function bleApp (central) {
                             gasSensor1.write('0xbb50', '0xbb52', {period: 255}, function (err) {
                                 if (err) 
                                     console.log(chalk.red('[         error ]') + ' failed to change the period. ' + err);
-                                else 
-                                    console.log('[ debug message ] changed the reporting period to 2.55s.'); // (recommend range: 100-255)
-                            });                              																		
+                                else {
+                                    gasSensor.read('0xbb50', '0xbb52', function (err, value) {
+                                        if (err)
+                                            console.log(chalk.red('[         error ]') + ' failed to read period. ' + err);
+                                        else
+                                            console.log('[ debug message ] changed the reporting period to ' + value.period / 100 + 's.'); // (recommend range: 100-255)
+                                    });
+                                }
+                            });
                             break;
 
-                        case '0x20914838225b':                                                    
-                            //  write your application for the 2nd gas sensor  //                        
-							gasSensor2 = dev;    	
-                            configNotifyAll(gasSensor2);	
+                        case '0x20914838225b':
+                            //  write your application for the 2nd gas sensor  //
+							gasSensor2 = dev;
+                            configNotifyAll(gasSensor2);
                             gasSensor2.onNotified('0xbb60', '0xcc28', buzzerHdlr); // buzzer
-							gasSensor2.onNotified('0xbb50', '0xcc04', gasHdlr);    // gas                           					
+							gasSensor2.onNotified('0xbb50', '0xcc04', gasHdlr);    // gas
                             gasSensor2.write('0xbb50', '0xbb52', {period: 255}, function (err) {
-                                if (err) 
+                                if (err)
                                     console.log(chalk.red('[         error ]') + ' failed to change the period. ' + err);
-                                else 
-                                    console.log('[ debug message ] changed the reporting period to 2.55s.'); // (recommend range: 100-255)
+                                else {
+                                    gasSensor.read('0xbb50', '0xbb52', function (err, value) {
+                                        if (err)
+                                            console.log(chalk.red('[         error ]') + ' failed to read period. ' + err);
+                                        else
+                                            console.log('[ debug message ] changed the reporting period to ' + value.period / 100 + 's.'); // (recommend range: 100-255)
+                                    });
+                                }
                             });
 
 							break;
                     }
- */  
+ */
 				}
-				break;    
+				break;
 
             /***   devStatus     ***/
             case 'devStatus':
@@ -139,15 +157,15 @@ function bleApp (central) {
 				console.log('[    devLeaving ]' + '@' + dev.addr);
 				break;
 
-            /***   attrsChange   ***/                
+            /***   attrsChange   ***/
 			case 'attChange':
                 //console.log('[   attrsChange ] ' + '@' + dev.addr + ', ' + dev.name + ', ' + msg.data.sid.uuid + ', ' + msg.data.cid.uuid + ', ' + JSON.stringify(msg.data.value));  // print all attribute changes once received.
 				break;
-            /***   attNotify     ***/                   
-			case 'attNotify':				
+            /***   attNotify     ***/
+			case 'attNotify':
 				break;
-                
-            /***   devNeedPasskey   ***/  
+
+            /***   devNeedPasskey   ***/
 			case 'devNeedPasskey':
 				// cc-bnp only
 				console.log('[devNeedPasskey ]');
@@ -157,11 +175,11 @@ function bleApp (central) {
 }
 
 /*****************************************************/
-/*    Gas Alarm Sensor Callback Handler              */ 
+/*    Gas Alarm Sensor Callback Handler              */
 /*****************************************************/
 function buzzerHdlr(data) {
     // show buzzer state
-	console.log('[ debug message ] Buzzer state: ' + data.onOff);
+	console.log('[ debug message ] Buzzer State: ' + data.onOff);
 	/***  write your application here   ***/
 }
 
@@ -185,7 +203,7 @@ function callbackAIn(data) {
 
 /**********************************/
 /* Private Utility Function       */
-/**********************************/  
+/**********************************/
 function configNotifyAll(dev) {
 	var devData = {
 		permAddr: dev.addr,
